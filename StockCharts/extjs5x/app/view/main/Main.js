45c01068b1d239d656446ae08ab50742815ae4fc
Ajit Kumar <ajit.kumar@walkingtree.in>
2014-08-23 10:49:36 +0530

    Added William %R Indicator chart for ExtJS 5. All files, including SDK, have been committed.

---
+++ StockCharts/extjs5x/app/view/main/Main.js
@@ -0,0 +1,27 @@
+/**
+ * This class is the main view for the application. It is specified in app.js as the
+ * "autoCreateViewport" property. That setting automatically applies the "viewport"
+ * plugin to promote that instance of this class to the body element.
+ *
+ * TODO - Replace this content of this view to suite the needs of your application.
+ */
+Ext.define('Chartsly.view.main.Main', {
+    extend: 'Ext.container.Container',
+
+    requires: ['Chartsly.view.test.WilliamPctR'],
+
+    xtype: 'app-main',
+    
+    controller: 'main',
+    viewModel: {
+        type: 'main'
+    },
+
+    layout: {
+        type: 'fit'
+    },
+
+    items: [{
+        xclass: 'Chartsly.view.test.WilliamPctR'
+    }]
+});
