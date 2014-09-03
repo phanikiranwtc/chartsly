038101ca9c2093b538d4ea9d6b44007e0688ac66
Ajit Kumar <ajit.kumar@walkingtree.in>
2014-09-03 23:31:02 +0530

    Added SimpleMovingAverage (SMA) overlay for ExtJS and Touch

--- StockCharts/extjs5x/app/view/test/Overlay.js
+++ StockCharts/extjs5x/app/view/test/Overlay.js
@@ -5,14 +5,16 @@ Ext.define("Chartsly.view.test.Overlay", {
     extend: 'Ext.Panel',
     requires: [
         // 'Chartsly.view.test.CandleStick'
-        'Chartsly.view.test.ParabolicSAR'
+        // 'Chartsly.view.test.ParabolicSAR'
+        'Chartsly.view.test.SimpleMovingAverage'
     ],
     config: {
         layout: 'fit',
         items: [
             {
-                // xtype: 'candlestick-test-chart',
-                xtype: 'cs-psar-test-chart'
+                // xtype: 'candlestick-test-chart'
+                // xtype: 'cs-psar-test-chart'
+                xtype: 'cs-sma-test-chart'
             }
         ]
     }
