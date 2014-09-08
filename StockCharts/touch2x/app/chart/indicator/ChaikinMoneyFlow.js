672d275b15340734115f28d400b01753d649c558
Ranjit <ranjit.n.battewad@walkingtree.in>
2014-09-08 09:25:02 +0530

    Pushing the files for Chikin Money Flow indicator for the touch version refs #30587

---
+++ StockCharts/touch2x/app/chart/indicator/ChaikinMoneyFlow.js
@@ -0,0 +1,34 @@
+/**
+ * @class Chartsly.chart.indicator.ChaikinMoneyFlow
+ * @extends Ext.chart.CartesianChart
+ *
+ * ChaikinMoneyFlow chart that looks for numeric axis and adds Chikin MF specific configuration
+ * such as fields, maximum, and minimum. fields is defaulted to ['pctr'] as this field is added by the
+ * ChaikinMoneyFlow series to the records.
+ * 
+ * The calculated %R value is set a "cmf" field on the record
+ */
+Ext.define("Chartsly.chart.indicator.ChaikinMoneyFlow", {
+    extend: 'Ext.chart.CartesianChart',
+    requires: ['Chartsly.series.indicator.ChaikinMoneyFlow', 'Chartsly.sprite.indicator.ChaikinMoneyFlow'],
+    xtype: 'chaikinmoneyflow',
+
+    initConfig: function(config) {
+		 
+        var series = config.series[0];
+       
+        Ext.Array.each(config.axes, function(axis, index, recs) {
+            if (axis.type === 'numeric') {
+                Ext.apply(axis, {
+                    fields: ['cmf'],
+                    maximum: 1,
+                    minimum: -1
+						  //,majorTickSteps:4
+                 
+                });
+            }
+        });
+
+        this.callParent(arguments);
+    }
+});
---
+++ StockCharts/touch2x/app/view/test/ChaikinMoneyFlow.js
@@ -0,0 +1,82 @@
+/**
+ * Class to test Chikin Money Flow indicator  chart
+ */
+Ext.define("Chartsly.view.test.ChaikinMoneyFlow", {
+    extend: 'Ext.Panel',
+    requires: [
+        'Chartsly.view.test.CandleStick',
+        'Ext.chart.axis.Time',
+        'Ext.chart.axis.Numeric',
+        'Ext.chart.series.Line',
+        'Chartsly.chart.indicator.ChaikinMoneyFlow',
+        'Chartsly.model.Stock', 
+        'Chartsly.store.Apple'
+    ],
+    config: {
+        items: [
+            {
+                xtype: 'candlestick-test-chart',
+                height: 350,
+            },
+            {
+                xclass: 'Chartsly.chart.indicator.ChaikinMoneyFlow',
+                height: 250,
+                insetPadding: {
+                    top: 10,
+                    right: 0,
+                    left: 0,
+                    bottom: 0
+                },
+                background: 'white',
+                series: [
+                    {
+                        store: Ext.create('Chartsly.store.Apple', {}), //'Apple',
+                        type: 'chaikinmoneyflow',
+                        xField: 'date',
+                        yField: 'cmf',
+                        highField: "high",
+                        lowField: "low",
+                        closeField: "close",
+								volumeField :"volume",
+                        chaikinMoneyFlowPeriod: 20,  //in days
+                        style: {
+                            stroke: 'rgba(237,123,43,0.75)',
+                            fill: 'rgba(237,123,43,0.1)',
+                            miterLimit: 1
+                        }
+                    }
+                ],
+                axes: [
+                    {
+                        type: 'numeric',
+                        position: 'left',
+                        style: {
+                            axisLine: false
+                        }
+                    },
+                    {
+                        type: 'time',
+                        position: 'bottom',
+                        fields: ['date'],
+                        style: {
+                            strokeStyle: '#666',
+                            estStepSize: 150
+                        },
+                        dateFormat: 'Y',
+                        segmenter: {
+                            type: 'time',
+                            step: {
+                                unit: 'y',
+                                step: 1
+                            }
+                        },
+                        label: {
+                            fontSize: 10,
+                            fillStyle: '#666'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+});
