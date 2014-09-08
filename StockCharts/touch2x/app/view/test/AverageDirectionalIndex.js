975f8545206bdf549e1d2c175377d151b38d2420
walkingtree <walkingtree@localhost.localdomain>
2014-09-08 11:16:42 +0530

    Average Directional index refs #30370

---
+++ StockCharts/touch2x/app/view/test/AverageDirectionalIndex.js
@@ -0,0 +1,87 @@
+/**
+ * Class to test AverageDirectionalIndex chart
+ */
+Ext.define("Chartsly.view.test.AverageDirectionalIndex", {
+    extend: 'Ext.Panel',
+    requires: [
+        'Chartsly.view.test.CandleStick',
+        'Ext.chart.axis.Time',
+        'Ext.chart.axis.Numeric',
+        'Ext.chart.series.Line',
+        'Chartsly.chart.indicator.AverageDirectionalIndex',
+        'Chartsly.model.Stock', 
+        'Chartsly.store.Apple'
+    ],
+    config: {
+        items: [
+            {
+                xtype: 'candlestick-test-chart',
+                height: 350,
+                  insetPadding: {
+                    top: 10,
+                    right: 10,
+                    left: 10,
+                    bottom: 10
+                },
+            },
+            {
+                xclass: 'Chartsly.chart.indicator.AverageDirectionalIndex',
+                height: 250,
+                insetPadding: {
+                    top: 10,
+                    right: 10,
+                    left: 10,
+                    bottom: 10
+                },
+                background: 'white',
+                series: [
+                    {
+                        store: Ext.create('Chartsly.store.Apple', {}), //'Apple',
+                        type: 'adx',
+                        xField: 'date',
+                        yField: 'ADX',
+                        highField: "high",
+                        lowField: "low",
+                        closeField: "close",smooth:true,
+                        lookBackPeriod: 14,  //in days
+                        style: {
+                            stroke: 'black',
+                            fill: 'rgba(237,123,43,0.1)',
+                            miterLimit: 1
+                        }
+                    }
+                ],
+                axes: [
+                    {
+                        type: 'numeric',
+                        position: 'left',
+                        style: {
+                            axisLine: false
+                        }
+                    },
+                    {
+                        type: 'time',
+                        position: 'bottom',
+                        fields: ['Time'],
+                        style: {
+                            strokeStyle: '#666',
+                            estStepSize: 150
+                        },
+                        dateFormat: 'Y',
+                        segmenter: {
+                            type: 'time',
+                            step: {
+                                unit: 'y',
+                                step: 1
+                            }
+                        },
+                        label: {
+                            fontSize: 10,
+                            fillStyle: '#666'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+});
