c729c96c0314d2170073283553a50793f68f4e52
Ranjit <ranjit.n.battewad@walkingtree.in>
2014-09-18 12:40:11 +0530

    Modified CMF indicator to handle the case where cmv value is already calculated at server side and provided in store.
    Created separate store and model, "AppleServer" store has cmf value
    
    - Chartsly.model.StockServer
    - Chartsly.store.AppleServer
    
    Added new Helper class for the Chartsly application. (Chartsly.util.Helper)

---
+++ StockCharts/extjs5x/app/util/Helper.js
@@ -0,0 +1,31 @@
+/**
+ * This class is helper class for the "Chartsly" application.
+ *
+ */
+Ext.define('Chartsly.util.Helper', {
+    singleton : true,
+	 alias: 'helper',
+
+	 /**
+	  *  @param {Ext.data.Store} Store used for series
+	  *  @param fieldName	y-field for the series, which is used to check whether the store 
+	  *							(associated model) has this field or not
+	  */
+    checkForFieldPresence: function (store, fieldName) {
+		
+		 var fieldPresent = false;
+		 
+		 if(! Ext.isEmpty(store) && ! Ext.isEmpty(fieldName) ){
+		 	
+	 	  		var modelFieldsArray = store.getModel().getFields();
+	 	  		var fieldNamesArray = Ext.Array.pluck(modelFieldsArray, "name");
+	  
+	 	 		fieldPresent =  Ext.Array.contains(fieldNamesArray,fieldName);
+			
+		 }
+	  	
+		 return fieldPresent;
+    }
+
+    
+});
