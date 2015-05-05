be1a52e89cefc3f81b84cf5d1adf1f36f6212ba9
chandrakalametta <chandrakalametta@users.noreply.github.com>
2015-05-05 14:46:45 +0530

    Annotations are not moved as per the requirement refs #34530

--- StockCharts/extjs6x/app/interactions/Annotation.js
+++ StockCharts/extjs6x/app/interactions/Annotation.js
@@ -229,7 +229,9 @@ Ext.define('Chartsly.interactions.Annotation', {
 
         surface.renderFrame();
         me.unlockEvents(me.getMoveGesture());
-
+        if(!Ext.isDefined(me.itemOnMove)){
+             me.itemOnMove = me.getMatchingAnnotationSprite(e);
+        }
         chart.fireEvent('annotationmoved', chart, me.itemOnMove.sprite, e);
     },
 
@@ -305,6 +307,9 @@ Ext.define('Chartsly.interactions.Annotation', {
                 // console.log('Found a matching annotation sprite. Returning......');
                 return tmp;
             }
+            if(!Ext.isDefined(me.itemOnMove)){
+                    return tmp;
+             }
         }
 
         //if we are here, we could not find anything
